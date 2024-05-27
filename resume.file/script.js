59bb251ee8953de6b0663d060bffe02a00aedb85
Aaron Cole <145709167+arrow-cole@users.noreply.github.com>
2024-05-27 19:50:06 -0400

    Create script.js

---
+++ resume.file/script.js
@@ -0,0 +1,23 @@
+const users = [
+    { username: 'admin', password: 'City8map' },
+    { username: 'boss', password: 'pass4' }
+];
+
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    const messageDiv = document.getElementById('message');
+    if (user) {
+        messageDiv.textContent = 'Login successful!';
+        messageDiv.style.color = 'green';
+            window.location.href = 'resume.html';
+    } else {
+        messageDiv.textContent = 'Invalid username or password!';
+        messageDiv.style.color = 'red';
+    }
+});
