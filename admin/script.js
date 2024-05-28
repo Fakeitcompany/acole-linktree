e4c46aa40122beb25a8e4581a10b054caf734ae8
Aaron Cole <145709167+arrow-cole@users.noreply.github.com>
2024-05-28 07:27:15 -0400

    Update admin

---
+++ admin/script.js
@@ -0,0 +1,35 @@
+const users = [
+    { username: 'admin', password: 'City8map', access: ['*'] },
+    { username: 'users', password: 'acole123', access: ['*'] },
+    { username: 'test', password: 'test12321', access: ['testing.html'] },
+    { username: 'interviewer', password: 'work2024', access: ['resume.file/resume.html'] }
+];
+
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    const messageDiv = document.getElementById('message');
+    if (user) {
+        messageDiv.textContent = 'Login successful!';
+        messageDiv.style.color = 'green';
+
+        // Check if the user is 'interviewer' to redirect accordingly
+        if (username === 'interviewer') {
+            // Redirect to specific pages
+            window.location.href = 'resume.file/resume.html';
+             } else if (username === 'admin') {
+            window.location.href = 'admin/index.html';
+        } else {
+            // For other users, redirect to a generic private page
+            window.location.href = 'private.html';
+        }
+    } else {
+        messageDiv.textContent = 'Invalid username or password!';
+        messageDiv.style.color = 'red';
+    }
+});
