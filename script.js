ccee395619813e8b0da72af0bf8050716d528d01
Aaron Cole <145709167+arrow-cole@users.noreply.github.com>
2024-05-27 20:30:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,14 +23,8 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
         if (username === 'interviewer') {
             // Redirect to specific pages
             window.location.href = 'resume.file/resume.html';
-         // Check if the user is 'test' to redirect accordingly
-        if (username === 'test') {
-            // Redirect to specific pages
-            window.location.href = 'test.html';  
-         // Check if the user is 'admin' to redirect accordingly
-        if (username === 'admin') {
-            // Redirect to specific pages
-            window.location.href = 'private.html';    
+      
+    
         } else {
             // For other users, redirect to a generic private page
             window.location.href = 'private.html';
