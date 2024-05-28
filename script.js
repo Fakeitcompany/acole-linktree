26f82b37507692cc66e6b1949748f57a6dd75208
Aaron Cole <145709167+arrow-cole@users.noreply.github.com>
2024-05-28 07:22:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const users = [
     { username: 'admin', password: 'City8map', access: ['*'] },
     { username: 'users', password: 'acole123', access: ['*'] },
     { username: 'test', password: 'test12321', access: ['testing.html'] },
-    { username: 'interviewer', password: 'work2024', access: ['resume.file/resume.html'] };
-    { username: 'aaron', password:'aaronya5', access: ['*'] }
+    { username: 'interviewer', password: 'work2024', access: ['resume.file/resume.html'] }
 ];
 
 document.getElementById('loginForm').addEventListener('submit', function (e) {
